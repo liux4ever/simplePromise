c56249aa5064598399ca86cd5e003dc252819323
liux4ever <liux4ever@gmail.com>
2021-07-14 00:08:17 +0800

    done

--- promise-class.js
+++ promise-class.js
@@ -56,32 +56,36 @@ class MyPromise {
     const realOnFullfilled = this.isFunction(onFullfilled)
       ? onFullfilled
       : (value) => {
-          return value;
-        };
+        return value;
+      };
     const realOnRejected = this.isFunction(onRejected)
       ? onRejected
       : (reason) => {
-          throw reason;
-        };
+        throw reason;
+      };
     // then的返回值整体是一个promise
     const promise2 = new MyPromise((resolve, reject) => {
       // onFullfilled 或 onRejected 异常时，promise reject
       const fullfilledMicrotask = () => {
-        try {
-          // onFullfilled 或 onRejected 执行结果为x，调用resolvePromise
-          const x = realOnFullfilled(this.value);
-          this.resolvePromise(promise2, x, resolve, reject);
-        } catch (e) {
-          reject(e);
-        }
+        queueMicrotask(() => {
+          try {
+            // onFullfilled 或 onRejected 执行结果为x，调用resolvePromise
+            const x = realOnFullfilled(this.value);
+            this.resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        })
       };
       const rejectedMicrotask = () => {
-        try {
-          const x = realOnRejected(this.reason);
-          this.resolvePromise(promise2, x, resolve, reject);
-        } catch (e) {
-          reject(e);
-        }
+        queueMicrotask(() => {
+          try {
+            const x = realOnRejected(this.reason);
+            this.resolvePromise(promise2, x, resolve, reject);
+          } catch (e) {
+            reject(e);
+          }
+        })
       };
       // 调用then时,不同状态不同回调
       switch (this.status) {
@@ -101,21 +105,125 @@ class MyPromise {
     });
     return promise2;
   }
+  catch(onRejected) {
+    this.then(null, onRejected)
+  }
+
   resolvePromise(promise2, x, resolve, reject) {
     if (promise2 === x) {
       return reject(
         new TypeError("the Promise and the return value are the same")
       );
     }
-    // 如果x是一个promise，让新promise接收x
+    // 如果x是一个promise，让新promise接收x，即继续执行x
     if (x instanceof MyPromise) {
+      queueMicrotask(() => {
+        x.then((y) => {
+          this.resolvePromise(promise2, y, resolve, reject)
+        })
+      })
+    } else if (typeof x == 'object' || this.isFunction(x)) {
+      if (x == null) {
+        return resolve(x)
+      }
+      let then = null
+      try {
+        then = x.then
+      } catch (e) {
+        return reject(e)
+      }
+      // 如果then是一个函数
+      if (this.isFunction(then)) {
+        let called = false;
+        try {
+          then.call(x, (y) => {
+            if (called) {
+              return
+            }
+            called = true
+            this.resolvePromise(promise2, y, resolve, reject)
+          }, (r) => {
+            if (called) {
+              return
+            }
+            called = true
+            reject(r)
+          })
+        } catch (e) {
+          if (called) {
+            return
+          }
+          reject(e)
+        }
+      } else {
+        resolve(x)
+      }
+    } else {
+      resolve(x)
     }
   }
   // 判断是否是一个函数
   isFunction(val) {
     return typeof val == "function";
   }
+
+  static resolve(value) {
+    if (value instanceof MyPromise) {
+      return value
+    }
+    return new MyPromise((resolve) => {
+      resolve(value)
+    })
+  }
+
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+
+  static race(list) {
+    return new MyPromise((resolve, reject) => {
+      const length = list.length;
+      if (!length) {
+        return resolve()
+      } else {
+        for (let i = 0; i < length; i++) {
+          MyPromise.resolve(list[i]).then(
+            val => {
+              return resolve(val),
+                reason => {
+                  return reject(reason)
+                }
+            })
+        }
+      }
+    })
+  }
 }
 
 // 实例化
-// const promise = new MyPromise((resolve, reject) => {});
+const promise = new MyPromise((resolve, reject) => {
+  resolve(111)
+})
+promise.then(val => { console.log('111') })
+promise.then(val => { console.log('222') })
+
+// const test1 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(111)
+//   }, 1000)
+// })
+// const test2 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(222)
+//   }, 2000)
+// })
+// const test3 = new MyPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(333)
+//   }, 3000)
+// })
+// MyPromise.race([test1, test2, test3]).then(val => {
+//   console.log(val)
+// })
